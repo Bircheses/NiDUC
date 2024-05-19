ae8e11682dca3822b75d4c6993bb855662bbf965
Bircheses <kbrzoza03@outlook.com>
2024-05-19 18:01:54 +0200

    Coder(15,5)

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@ using namespace std;
 void showVector(vector<int>& vec);
 vector<int> polynomialDivison(vector<int>& divisor, vector<int>& dividend);
 vector<int> coder_15_7(vector<int>& information);
+vector<int> coder_15_5(vector<int>& information);
 
 int main() {
     vector<int> information = {1,0,1,1,1,0,1};
     vector<int> coded_information = coder_15_7(information);
+    showVector(coded_information);
+    information = {1,0,1,1,0};
+    coded_information = coder_15_5(information);
     showVector(coded_information);
     return 0;
 }
@@ -35,6 +39,19 @@ void showVector(vector<int>& vec){
 vector<int> coder_15_7(vector<int>& information){
     vector<int> generator_polynomial = {1,1,1,0,1,0,0,0,1};
     for(int i=0; i<8; i++) information.push_back(0);
+    vector<int> coded_information;
+    coded_information.resize(information.size());
+    vector<int> remainder = polynomialDivison(generator_polynomial, information);
+    for(int i=0; i<=information.size(); i++){
+        if(i<=remainder.size()) coded_information[coded_information.size()-i]=(information[information.size()-i]+remainder[remainder.size()-i])%2;
+        else coded_information[coded_information.size()-i]=information[information.size()-i];
+    }
+    return coded_information;
+}
+
+vector<int> coder_15_5(vector<int>& information){
+    vector<int> generator_polynomial = {1,0,1,0,0,1,1,0,1,1,1};
+    for(int i=0; i<10; i++) information.push_back(0);
     vector<int> coded_information;
     coded_information.resize(information.size());
     vector<int> remainder = polynomialDivison(generator_polynomial, information);
