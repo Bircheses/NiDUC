2f9e28a44105172907e119667bee986867dc4191
Bircheses <kbrzoza03@outlook.com>
2024-05-19 15:40:28 +0200

    Coder(15,7)

--- main.cpp
+++ main.cpp
@@ -4,34 +4,43 @@
 using namespace std;
 
 void showVector(vector<int>& vec);
-void polynomialDivison(vector<int>& divisor, vector<int>& dividend, vector<int>& quotient, vector<int>& remainder);
+vector<int> polynomialDivison(vector<int>& divisor, vector<int>& dividend);
+vector<int> coder_15_7(vector<int>& information);
 
 int main() {
-    vector<int> divisor = {1,0,1,1};
-    vector<int> dividend = {1,1,0,0,1,0,1};
-    vector<int> quotient;
-    vector<int> remainder;
-
-    polynomialDivison(divisor, dividend, quotient, remainder);
-
+    vector<int> information = {1,0,1,1,1,0,1};
+    vector<int> coded_information = coder_15_7(information);
+    showVector(coded_information);
     return 0;
 }
 
-void polynomialDivison(vector<int>& divisor, vector<int>& dividend, vector<int>& quotient, vector<int>& remainder){
+vector<int> polynomialDivison(vector<int>& divisor, vector<int>& dividend){
+    vector<int> quotient, remainder;
     quotient.resize(dividend.size()-divisor.size()+1);
     copy(dividend.begin(), dividend.end(), back_inserter(remainder));
-
     while(remainder.size()>=divisor.size()) {
         quotient[quotient.size()-(remainder.size()-divisor.size())-1]=1;
         for (int i = 0; i < divisor.size(); i++) remainder[i] = (remainder[i] + divisor[i]) % 2;
         do remainder.erase(remainder.begin());
         while (remainder[0]==0 && !remainder.empty());
     }
-    showVector(quotient);
-    showVector(remainder);
+    return remainder;
 }
 
 void showVector(vector<int>& vec){
     for(int number:vec) cout << number;
     cout << endl;
+}
+
+vector<int> coder_15_7(vector<int>& information){
+    vector<int> generator_polynomial = {1,1,1,0,1,0,0,0,1};
+    for(int i=0; i<8; i++) information.push_back(0);
+    vector<int> coded_information;
+    coded_information.resize(information.size());
+    vector<int> remainder = polynomialDivison(generator_polynomial, information);
+    for(int i=0; i<=information.size(); i++){
+        if(i<=remainder.size()) coded_information[coded_information.size()-i]=(information[information.size()-i]+remainder[remainder.size()-i])%2;
+        else coded_information[coded_information.size()-i]=information[information.size()-i];
+    }
+    return coded_information;
 }
